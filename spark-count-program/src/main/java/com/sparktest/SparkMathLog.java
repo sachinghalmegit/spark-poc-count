cafd7a8464f6a3e6619cf19e9a5cb1cb053c42be
sachinghalmegit <sachinghalme8@gmail.com>
2020-07-29 03:42:19 -0400

    Addd New Program and rename the existing file name

---
+++ spark-count-program/src/main/java/com/sparktest/SparkMathLog.java
@@ -0,0 +1,31 @@
+package com.sparktest;
+
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class SparkMathLog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SparkConf sparkConf = new SparkConf().setAppName("Read Text to RDD").setMaster("local[2]")
+				.set("spark.executor.memory", "2g");
+// start a spark context
+		JavaSparkContext sc = new JavaSparkContext(sparkConf);
+		sc.setLogLevel("ERROR");
+// initialize an integer RDD
+		JavaRDD<Integer> numbers = sc.parallelize(Arrays.asList(14, 21, 88, 99, 455));
+
+// map each line to number of words in the line
+		JavaRDD<Double> log_values = numbers.map(x -> Math.log(x));
+
+// collect RDD for printing
+		for (double value : log_values.collect()) {
+			System.out.println(value);
+		}
+		sc.close();
+	}
+
+}
--- spark-count-program/src/main/java/com/sparktest/SparkWorldCount.java
+++ spark-count-program/src/main/java/com/sparktest/SparkWorldCount.java
@@ -3,28 +3,28 @@ package com.sparktest;
 import java.util.Arrays;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-public class SparkCount {
+import scala.Tuple2;
+
+public class SparkWorldCount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SparkConf sparkConf = new SparkConf().setAppName("Read Text to RDD").setMaster("local[2]")
 				.set("spark.executor.memory", "2g");
-// start a spark context
+		// start a spark context
 		JavaSparkContext sc = new JavaSparkContext(sparkConf);
 		sc.setLogLevel("ERROR");
-// initialize an integer RDD
-		JavaRDD<Integer> numbers = sc.parallelize(Arrays.asList(14, 21, 88, 99, 455));
-
-// map each line to number of words in the line
-		JavaRDD<Double> log_values = numbers.map(x -> Math.log(x));
-
-// collect RDD for printing
-		for (double value : log_values.collect()) {
+		JavaRDD<String> textFile = sc.textFile("TextFile.txt");
+		JavaPairRDD<String, Integer> counts = textFile.flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+				.mapToPair(word -> new Tuple2<>(word, 1)).reduceByKey((a, b) -> a + b);
+		for (Tuple2<String, Integer> value : counts.collect()) {
 			System.out.println(value);
-		} 
+		}
+		sc.close();
 	}
 
 }
